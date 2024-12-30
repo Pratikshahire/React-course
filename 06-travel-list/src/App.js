038d71ec32fa8233362609e1c43463121c43eaac
Pratiksha <pratikshahire21@gmail.com>
2024-12-30 12:23:07 +0530

    updates to project6

--- 06-travel-list/src/App.js
+++ 06-travel-list/src/App.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Logo from "./Logo";
 import Form from "./Form";
 import PackingList from "./PackingList";
-import Stats from "./Stats";
+import Stats from "./Stats";  
 
 function App() {
   const [items, setItems] = useState([]);
